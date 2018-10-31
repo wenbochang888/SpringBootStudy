5b84b8fd46c228e73ad697730546efc01d40ef38
wenbochang888 <727987105@qq.com>
2018-10-31 11:37:37 +0800

    测试冲突

--- src/main/java/com/SpringbootstudyApplication.java
+++ src/main/java/com/SpringbootstudyApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootstudyApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("测试新开分支，并且合并到主master");
-		System.out.println("这里应该会出现冲突代码");
 		SpringApplication.run(SpringbootstudyApplication.class, args);
 	}
 }
